092f7883d80746f4b0d066bba451049b079d3b06
Jim <jim.pollaro@gmail.com>
2017-10-24 14:03:58 -0700

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024203134) do
+ActiveRecord::Schema.define(version: 20171024205818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171024203134) do
     t.json     "similar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "mb_id"
   end
 
   create_table "concerts", force: :cascade do |t|
