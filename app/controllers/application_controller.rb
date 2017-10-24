8d03cce84249ecbf6091ae546b8aa60b5b2e2e37
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-10-24 16:04:04 -0700

    merged conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  protect_from_forgery with: :exception
-
-  require 'date'
-=======
     protect_from_forgery with: :exception
 
     def current_user
@@ -11,5 +6,4 @@ class ApplicationController < ActionController::Base
     end
     helper_method :current_user
 
->>>>>>> 8ae9bd623db9e730465eb8a1d05ec78bad4c2d62
 end
--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,5 +1,5 @@
 class BandsController < ApplicationController
-<<<<<<< HEAD
+  
   require 'date'
   
   def index
@@ -15,22 +15,12 @@ class BandsController < ApplicationController
    
     @longitude = testvar["setlist"][0]["venue"]["city"]["coords"]["long"]
     @lat = testvar["setlist"][0]["venue"]["city"]["coords"]["lat"]
-=======
-
-  def index
-    response = HTTParty.get("https://api.setlist.fm/rest/1.0/search/setlists",:query => { :artistName => "Coldplay", :cityName => "Santa Clara", :date => "04-10-2017" },:headers => { "x-api-key" => "1128bdd4-2942-4334-b4fa-5cf725b57260","Accept" => "application/json" })
-    # binding.pry
-    testvar = JSON.parse(response.body)
-
-    # binding.pry
->>>>>>> 8ae9bd623db9e730465eb8a1d05ec78bad4c2d62
     @songlist = testvar["setlist"][0]["sets"]["set"]
     @bandname = testvar["setlist"][0]["artist"]["name"]
     @venue = testvar["setlist"][0]["venue"]
     @venuename = testvar["setlist"][0]["venue"]["name"]
     @venuelocation = testvar["setlist"][0]["venue"]["city"]["name"]
     @venuestate = testvar["setlist"][0]["venue"]["city"]["state"]
-<<<<<<< HEAD
     t = testvar["setlist"][0]["eventDate"]
     cdate = Date.strptime(t, "%d-%m-%Y" )
     
@@ -38,12 +28,6 @@ class BandsController < ApplicationController
     @eventdate = cdate.strftime("%A, %B %d %Y")
      
 
-=======
-    @eventdate = testvar["setlist"][0]["eventDate"]
-
-    @long = venue["city"]["coords"]["long"]
-    @lat = venue["city"]["coords"]["lat"]
->>>>>>> 8ae9bd623db9e730465eb8a1d05ec78bad4c2d62
   end
 
 
