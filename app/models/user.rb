ecf3dbb45da3e7fe8ddf12ea611dfe930332d6e1
LavenderIris <pchung@gmail.com>
2017-10-27 09:44:56 -0700

    got friends pages to show

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
     has_many :concerts
     has_many :bands
-    has_many :attends
+    has_many :attend
     has_many :wish
     has_many :concert_attended, through: :attend, source: :concerts
     has_many :band_wishes, through: :wish, source: :band
